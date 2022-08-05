b458dca83dab32c820bcddcd8d291fc1a3830c82
ankoyeeri <ray.zug@gmail.com>
2022-08-05 13:19:47 +0300

    feat: complete 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -43,7 +43,7 @@ function parseDataFromIso8601(/* value */) {
  * Returns true if specified date is leap year and false otherwise
  * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
  *
- * @param {date} date
+ * @param {Date} date
  * @return {bool}
  *
  * @example :
@@ -53,8 +53,14 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getUTCFullYear();
+
+  if (year % 4 !== 0) return false;
+  if (year % 100 !== 0) return true;
+  if (year % 400 !== 0) return false;
+
+  return true;
 }
 
 
@@ -73,8 +79,15 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const timespan = new Date(endDate - startDate);
+
+  const hours = timespan.getUTCHours() < 10 ? `0${timespan.getUTCHours()}` : timespan.getUTCHours();
+  const minutes = timespan.getMinutes() < 10 ? `0${timespan.getMinutes()}` : timespan.getMinutes();
+  const seconds = timespan.getSeconds() < 10 ? `0${timespan.getSeconds()}` : timespan.getSeconds();
+  const milliseconds = timespan.getMilliseconds() < 100 ? `00${timespan.getMilliseconds()}` : timespan.getMilliseconds();
+
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 
@@ -85,7 +98,7 @@ function timeSpanToString(/* startDate, endDate */) {
  *
  * SMALL TIP: convert to radians just once, before return in order to not lost precision
  *
- * @param {date} date
+ * @param {Date} date
  * @return {number}
  *
  * @example:
@@ -95,6 +108,12 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(/* date */) {
+  // let hours = new Date(date).getUTCHours();
+  // if (hours > 12) hours = hours % 12 || 12;
+
+  // const minutes = new Date(date).getUTCMinutes();
+
+  // return Math.abs(0.5 * (60 * hours - 11 * minutes)) * (Math.PI / 180);
   throw new Error('Not implemented');
 }
 
